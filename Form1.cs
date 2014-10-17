52269f05841d46b455bc63239c406bde054919c2
Alex Wong <alexwong@bu.edu>
2014-10-17 10:07:48 -0400

    Add some beginning components and Queen class

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace eight_queens
 {
     public partial class Form1 : Form
     {
+        private ArrayList Queens = new ArrayList();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            const int CELLHEIGHT = 50;
+            const int CELLWIDTH = 50;
+
+            Graphics g = e.Graphics;
+
+
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Queens.Clear();
+            this.Invalidate();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Queen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Queen
+{
+    public int xloc;
+    public int yloc;
+
+	public Queen(int x_in, int y_in)
+	{
+        xloc = x_in;
+        yloc = y_in;
+	}
+}
